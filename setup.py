253336f58968d7bcf619230fd619b598f57b05ac
BIKRANT-RAWAT <connect.bikrantrawat@gmail.com>
2026-04-16 18:55:59 +0530

    requirements updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="First Aid MedBot",
+    version="0.1.0",
+    author="Bikrant Rawat",
+    author_email="connect.bikrantrawat@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
